8e100aff8b5b51f556dc6270677449d7a84250c7
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-04-14 19:14:54 +0200

    add layout for challenges

--- config.rb
+++ config.rb
@@ -12,6 +12,8 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/challenges/*', layout: "challenges_layout"
+
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
